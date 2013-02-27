88ddf84b8ee4588a54ff3a4447d4dd82ea4a0cee
Libor Krzyzanek <libor@krzyzanek.com>
2013-02-27 10:21:33 +0100

    Updated License info

--- src/main/java/org/jboss/community/sbs/plugin/gravatar/GravatarManagerImpl.java
+++ src/main/java/org/jboss/community/sbs/plugin/gravatar/GravatarManagerImpl.java
@@ -1,23 +1,7 @@
 /*
- * JBoss Community http://jboss.org/
- *
- * Copyright (c) 2010 Red Hat Middleware, LLC. All rights reserved.
- *
- * This copyrighted material is made available to anyone wishing to use,
- * modify, copy, or redistribute it subject to the terms and conditions
- * of the GNU Lesser General Public License, v. 2.1.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT A WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License, v.2.1 along with this distribution; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA.
- *
- * Red Hat Author(s): Libor Krzyzanek
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
  */
 package org.jboss.community.sbs.plugin.gravatar;
 
